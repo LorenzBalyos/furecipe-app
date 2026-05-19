3f7ddb2cc1ae639f371c1b7981dce70c637fa8e2
lorence <lorenzbalyos@gmail.com>
2026-05-20 07:21:06 +0800

    Fix entrypoint line endings to LF for Linux compatibility

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-# Safely clear the internal caches at boot time
 php artisan config:clear
 php artisan cache:clear
 
-# Run database migrations
 php artisan migrate --force
 
-# Start apache web server normally
 exec apache2-foreground
